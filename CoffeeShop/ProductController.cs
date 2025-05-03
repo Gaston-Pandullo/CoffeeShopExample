0d3e6e660426f88531f8225407192b4107754774
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-03 18:32:37 -0300

    Creacion de menu.

---
+++ CoffeeShop/ProductController.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CoffeeShop
+{
+    internal class ProductController
+    {
+        internal static void ActualizarProducto()
+        {
+            return;
+        }
+
+        internal static void AgregarProducto()
+        {
+            return;
+        }
+
+        internal static void BorrarProducto()
+        {
+            return;
+        }
+
+        internal static void VerProducto()
+        {
+            return;
+        }
+
+        internal static void VerTodosLosProducto()
+        {
+            return;
+        }
+    }
+}
--- CoffeeShop/Program.cs
+++ CoffeeShop/Program.cs
@@ -1,4 +1,45 @@
-﻿
+﻿using CoffeeShop;
+using Spectre.Console; // Libreria para generar una Menu interactivo.
+
+bool isAppRunning = true;
+
+while (isAppRunning)
+{
+    // Menu
+    var option = AnsiConsole.Prompt(
+    new SelectionPrompt<OpcionesMenu>()
+    .Title("¿Que quiere hacer?")
+    .AddChoices(
+        OpcionesMenu.AgregarProducto,
+        OpcionesMenu.BorrarProducto,
+        OpcionesMenu.ActualizarProducto,
+        OpcionesMenu.VerProducto,
+        OpcionesMenu.VerTodosLosProductos,
+        OpcionesMenu.Salir));
+
+    switch (option)
+    {
+        case OpcionesMenu.AgregarProducto:
+            ProductController.AgregarProducto();
+            break;
+        case OpcionesMenu.BorrarProducto:
+            ProductController.BorrarProducto();
+            break;
+        case OpcionesMenu.ActualizarProducto:
+            ProductController.ActualizarProducto();
+            break;
+        case OpcionesMenu.VerProducto:
+            ProductController.VerProducto();
+            break;
+        case OpcionesMenu.VerTodosLosProductos:
+            ProductController.VerTodosLosProducto();
+            break;
+        default:
+            break;
+    }
+}
+
+
 enum OpcionesMenu
 {
     AgregarProducto,
