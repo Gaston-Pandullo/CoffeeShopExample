6043a7c1a8e33f4de0dae008d2f4bea652dc4cd7
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-04 16:44:58 -0300

    Creacion de conexion a base de datos.

---
+++ CoffeeShop/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop
+{
+    internal class ProductContext: DbContext
+    {
+        // Tabla de productos
+        DbSet<Product> Products { set; get; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite($"Data Source= product.db");
+    }
+}
